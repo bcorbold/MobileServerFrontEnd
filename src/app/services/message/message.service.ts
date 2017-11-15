7428420d4aa9c882ea3278ea55fbb5bea46d2a1a
bcorbold <bradencorbold@gmail.com>
2017-11-15 13:17:32 -0500

    [core] Added placeholder for GET polling in the message service

--- src/app/services/message/message.service.ts
+++ src/app/services/message/message.service.ts
@@ -1,11 +1,28 @@
-import { Inject, Injectable } from '@angular/core';
+import { Inject, Injectable, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 import { AppConfig } from '../../app.config';
 
 @Injectable()
-export class MessageService {
+export class MessageService implements OnDestroy {
+  getPoller: Observable<any>; // todo(braden): Need to make this typed
 
-  constructor(@Inject(AppConfig) private config: AppConfig) { }
+  constructor(@Inject(AppConfig) private config: AppConfig) {
+    this.getPoller = Observable
+      .interval(5000)
+      .flatMap(() => {
+        return ['hi'];
+      });
+
+    this.getPoller.subscribe((message) => {
+      console.log(message.toString());
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.getPoller.unsubscribe();
+    this.getPoller = undefined;
+  }
 
   sendMessage() {
     console.log(`Our backend url: ${this.config.backendUrl}`);
