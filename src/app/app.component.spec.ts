a703628e0c969b49bff9c02f4bad27669e1d263a
bcorbold <bradencorbold@gmail.com>
2017-11-17 15:52:58 -0500

    [core] Fixed AppComponent test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,10 +24,11 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
+  it ('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
     expect(app).toBeTruthy();
-  }));
+  });
 
 });
