f63ded3d42279a28c025fb9a5763d75a4fe6995a
Jesse Tatasciore <jessetatasciore7@hotmail.com>
2017-11-21 13:22:32 -0500

    Import fixes

--- src/app/components/poc/poc.module.ts
+++ src/app/components/poc/poc.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { POCComponent } from './poc.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MessageModule, MessageService } from '../../services/message/message.module';
-import {POCFormModule} from '../../forms/poc/poc-form.module';
+import { POCFormModule } from '../../forms/poc/poc-form.module';
 
 export { POCComponent } from './poc.component';
 
