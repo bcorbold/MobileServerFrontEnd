9cccfac1bda65ed2e5c6ab24b3e996bce6428a7c
Jesse Tatasciore <jessetatasciore7@hotmail.com>
2017-11-26 17:58:57 -0500

    Add base files for bartender view

---
+++ src/app/components/bartender/bartender.component.ts
@@ -0,0 +1,135 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ms-bartender-component',
+  templateUrl: './bartender.component.html'
+})
+export class BartenderComponent {
+
+  batches = [
+    {
+      batchId: 1,
+      deadline: 60,
+      orders: [
+        {
+          orderId: 1,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 2
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 2,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 1
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 3,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 3
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 4,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 5
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        }
+      ]
+    },
+    {
+      batchId: 2,
+      deadline: 60,
+      orders: [
+        {
+          orderId: 5,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 2
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 6,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 1
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 7,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 3
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 8,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 5
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        }
+      ]
+    }
+  ];
+}
---
+++ src/app/components/bartender/bartender.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule, MatInputModule, MatSlideToggleModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { POCFormModule } from '../../forms/poc/poc-form.module';
+import { MessageModule, MessageService } from '../../services/message/message.module';
+import { BartenderComponent } from './bartender.component';
+
+export { BartenderComponent } from './bartender.component';
+
+@NgModule({
+  declarations: [
+    BartenderComponent
+  ],
+  exports: [
+    BartenderComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FlexLayoutModule,
+    MessageModule,
+    POCFormModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSlideToggleModule
+  ],
+  providers: [
+    MessageService
+  ]
+})
+export class BartenderModule { }
