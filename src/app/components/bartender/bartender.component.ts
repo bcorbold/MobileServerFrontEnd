64c300954670d45f8af7eab7c120ecc719f017a6
Jesse Tatasciore <jessetatasciore7@hotmail.com>
2017-11-29 18:55:38 -0500

    Fix the general layout issues

--- src/app/components/bartender/bartender.component.ts
+++ src/app/components/bartender/bartender.component.ts
@@ -131,6 +131,254 @@ export class BartenderComponent {
           }
         }
       ]
+    },
+    {
+      batchId: 2,
+      deadline: 60,
+      orders: [
+        {
+          orderId: 5,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 2
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 6,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 1
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 7,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 3
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 8,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 5
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        }
+      ]
+    },
+    {
+      batchId: 2,
+      deadline: 60,
+      orders: [
+        {
+          orderId: 5,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 2
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 6,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 1
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 7,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 3
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 8,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 5
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        }
+      ]
+    },
+    {
+      batchId: 2,
+      deadline: 60,
+      orders: [
+        {
+          orderId: 5,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 2
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 6,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 1
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 7,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 3
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 8,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 5
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        }
+      ]
+    },
+    {
+      batchId: 2,
+      deadline: 60,
+      orders: [
+        {
+          orderId: 5,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 2
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 6,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 1
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 7,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 3
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        },
+        {
+          orderId: 8,
+          orderInfo: {
+            addons: [
+              {
+                addonId: 1,
+                addonName: 'addon 1',
+                quantity: 5
+              }
+            ],
+            productId: 1,
+            productName: 'Product 1'
+          }
+        }
+      ]
     }
   ];
 }
